0b0cf8e497d02d9a55db48182a04809a4906fd92
xenophon <674550338@qq.com>
2024-05-04 22:33:09 +0800

    update lab2

---
+++ LAB2/dma.py
@@ -0,0 +1,114 @@
+from typing import List
+
+
+class DMA:
+    def __init__(self, size):
+        # 假设初始化时空间中每个地址都存储一个 # 符号
+        self.heap: List[str] = ["#"] * size
+        # 你可以在这里额外定义需要的数据结构
+        self.allocations: dict = {}
+        self.free_size: int = size
+
+    # 你需要实现以下三个 API
+    def malloc(self, id: int, size: int, value: list) -> bool:
+        if id in self.allocations:  # 这里的检查可以考虑删去
+            return False
+        if size > self.free_size:#一旦空闲大于需求，必须分配
+            return False
+        else:
+            start = self.find_free_space(size)
+            if start is None:
+                self.compact()# 确保整理后的大小能够继续分配
+            start = self.find_free_space(size)
+            self.allocate_memory(id, start, size, value)
+            self.free_size = self.free_size - size
+            return True
+
+    def free(self, id: int) -> bool:
+        """释放指定id的空间段"""
+        if id not in self.allocations:
+            return False
+
+        allocation = self.allocations[id]
+        start = allocation['start']
+        size = allocation['size']
+        self.free_size = self.free_size + size
+        self.free_memory(start, size)
+        del self.allocations[id]
+        return True
+        
+    def data(self) -> dict:
+        return self.allocations
+
+    # 除了上述 API 外，你可以额外定义必要的辅助函数
+    # 例如，通过调用 compact 来对内存空间进行碎片整理
+    def compact(self) -> None:
+        free_spaces = self.get_free_spaces()
+        if not free_spaces:
+            return
+
+        sorted_allocations = sorted(self.allocations.items(), key=lambda x: x[1]['start'])
+        new_heap = ['#'] * len(self.heap)
+        new_allocations = {}
+
+        current_start = 0
+        for id, allocation in sorted_allocations:
+            size = allocation['size']
+            value = allocation['value']
+            new_start = current_start
+            new_allocations[id] = {'start': new_start, 'size': size, 'value': value}
+
+            for i in range(size):
+                new_heap[new_start + i] = value[i]
+
+            current_start += size
+
+        self.heap = new_heap
+        self.allocations = new_allocations
+    
+
+    def find_free_space(self, size: int) -> int:
+        """
+        查找指定大小的空间，只查找
+        """
+        for i in range(len(self.heap) - size + 1):
+            if self.heap[i:i+size] == ['#'] * size:
+                return i
+        return None
+
+
+    def allocate_memory(self, id: int, start: int, size: int, value: list) -> None:
+        """分配指定段内存，只分配"""
+        self.allocations[id] = {'start': start, 'size': size, 'value': value}
+        for i in range(size):
+            self.heap[start + i] = value[i]
+
+    def free_memory(self, start: int, size: int):
+        """释放指定段内存，只释放"""
+        for i in range(size):
+            self.heap[start + i] = '#'
+
+
+    def get_free_spaces(self):
+        """获取空闲空间大小"""
+        free_spaces = []
+        start = None
+        for i, char in enumerate(self.heap):
+            if char == "#":
+                if start is None:
+                    start = i
+            else:
+                if start is not None:
+                    size = i - start
+                    free_spaces.append((start, size))
+                    start = None
+
+        if start is not None:
+            size = len(self.heap) - start
+            free_spaces.append((start, size))
+
+        return free_spaces
+
+if __name__=="__main__":
+    from test import main
+    main()
---
+++ LAB2/test.py
@@ -0,0 +1,66 @@
+import json
+import time
+from dma import DMA
+
+
+def execute_workload(workload):
+    heap_size = workload["size"]
+    requests = workload["requests"]
+    expected_results = workload["result"]
+    expected_assertions = workload["assert"]
+
+    # 初始化内存分配器
+    dma = DMA(heap_size)
+    start_time = time.time()
+    for i, request in enumerate(requests):
+        req_type = request["type"]
+        req_id = request["id"]
+
+        if req_type == "malloc":
+            req_size = request["size"]
+            req_value = request["value"]
+
+            result = dma.malloc(req_id, req_size, req_value)
+            assert result == expected_assertions[i]
+
+        elif req_type == "free":
+            result = dma.free(req_id)
+            assert result == expected_assertions[i]
+    end_time = time.time()
+
+    execution_time = end_time - start_time
+    # 检查heap中的分配结果
+    allocations = dma.data()
+    for expected_result in expected_results:
+        req_id = expected_result["id"]
+        req_size = expected_result["size"]
+        req_value = expected_result["value"]
+
+        assert req_id in allocations
+        allocation = allocations[req_id]
+        assert allocation["size"] == req_size
+        assert allocation["value"] == req_value
+    if execution_time > 0:
+        efficiency = len(requests) / execution_time
+    else:
+        efficiency = float('inf')  # 表示无穷大
+    print("Workload execution successful.")
+    print("Execution time: {:.4f} seconds".format(execution_time))
+    print(
+        "Efficiency: {:.2f} requests per second".format(efficiency)
+    )
+
+
+def main():
+    # 读取workload.json文件
+    with open("workload.json", "r") as f:
+        workloads = json.load(f)
+
+    # 遍历每个workload进行测试
+    for i in range(len(workloads)):
+        print("Executing workload #{}".format(i + 1))
+        workload = workloads[f"workload{i+1}"]
+        execute_workload(workload)
+
+if __name__=="__main__":
+    main()
